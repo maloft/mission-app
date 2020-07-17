2e7253731563cb6052032b4597fb91c890f2d1a3
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-17 16:06:53 +0200

    TP5.1 Spring Boot : Début

---
+++ src/main/java/dev/mission/repository/MissionRepository.java
@@ -0,0 +1,8 @@
+package dev.mission.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import dev.mission.entite.Mission;
+
+public interface MissionRepository extends JpaRepository<Mission, Integer> {
+
+}
